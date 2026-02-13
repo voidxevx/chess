807cfdcc6ffedfec10fdb8cbd70c72b28f01f984
voidxevx <rexbradbury123@gmail.com>
2026-02-13 09:40:54 -0800

    Added: renamed windows to widgets, more widget types.
    Created a test "player character" widget.

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,8 +1,7 @@
 mod controller;
-mod window;
+mod widget;
 
-use std::arch::x86_64::_store_mask16;
-use std::io::{self, Write, stdout, Cursor};
+use std::io::{self, Write, stdout};
 use std::sync::{Arc, Mutex};
 use crossterm::event::{self, Event, KeyCode, KeyEventKind, KeyModifiers};
 use crossterm::{execute, ExecutableCommand, QueueableCommand};
@@ -14,7 +13,7 @@ use crate::board::{
     deinit_board,
 };
 use crate::client::controller::{Dispatchable, Dispatcher};
-use crate::client::window::{GenericWindow, Window, Windowable};
+use crate::client::widget::{WidgetBuilder, WidgetType, Widget};
 
 #[allow(unsafe_op_in_unsafe_fn)]
 unsafe fn clean_application() -> io::Result<()> {
@@ -23,7 +22,7 @@ unsafe fn clean_application() -> io::Result<()> {
 
     // disable terminal raw mode.
     disable_raw_mode()?;
-    execute!(io::stdout(), Clear(ClearType::All), MoveTo(0, 0), Show, LeaveAlternateScreen)?;
+    execute!(stdout(), Clear(ClearType::All), MoveTo(0, 0), Show, LeaveAlternateScreen)?;
     Ok(())
 }
 
@@ -37,6 +36,10 @@ fn initialize_terminal() -> io::Result<()> {
 #[unsafe(no_mangle)]
 #[allow(unsafe_op_in_unsafe_fn)]
 pub unsafe extern "C" fn main_loop(local: bool) -> bool {
+    if local {
+        println!("Running local");
+    }
+
     ////////////////////////////////
     // APPLICATION INITIALIZATION //
     ////////////////////////////////
@@ -58,12 +61,12 @@ pub unsafe extern "C" fn main_loop(local: bool) -> bool {
     // STATE VARIABLES //
     /////////////////////
     let run_state = Arc::new(Mutex::new(true));
+    let mut widgets = Vec::new();
 
-    // event dispatcher
-    let mut dispatcher = Dispatcher::new();
+    let mut global_dispatcher = Dispatcher::new();
     // close game loop
     let run_state_handle = run_state.clone();
-    dispatcher.add_event_binding(Box::new(controller::KeyEvent::new(
+    global_dispatcher.add_event_binding(Box::new(controller::KeyEvent::new(
         KeyCode::Char('q'),
         KeyModifiers::NONE,
         KeyEventKind::Press,
@@ -75,15 +78,76 @@ pub unsafe extern "C" fn main_loop(local: bool) -> bool {
         }
     )));
 
-    let mut window = Window::Generic(
-        GenericWindow::new(
-            (70, 45),
-            (10, 5),
-            dispatcher,
-            run_state.clone(),
-            "Test Window".to_string(),
-        )
-    );
+    let mut global_widget = WidgetBuilder::new()
+        .win_type(WidgetType::GlobalInput)
+        .build();
+    global_widget.attach_dispatcher(global_dispatcher);
+    widgets.push(global_widget);
+
+    let mut moving_window = WidgetBuilder::new()
+        .win_type(WidgetType::Generic)
+        .size((3, 3))
+        .build();
+
+    let mut moving_window_controller = Dispatcher::new();
+    let moving_window_handle = moving_window.get_data_handle().expect("Couldn't get window handle");
+    moving_window_controller.add_event_binding(Box::new(controller::KeyEvent::new(
+        KeyCode::Left,
+        KeyModifiers::NONE,
+        KeyEventKind::Press,
+        true,
+        move || -> io::Result<()> {
+            let mut data_guard = moving_window_handle.lock().unwrap();
+            (*data_guard).position.0 -= 1;
+            drop(data_guard);
+            Ok(())
+        }
+    )));
+
+    let moving_window_handle = moving_window.get_data_handle().expect("Couldn't get window handle");
+    moving_window_controller.add_event_binding(Box::new(controller::KeyEvent::new(
+        KeyCode::Right,
+        KeyModifiers::NONE,
+        KeyEventKind::Press,
+        true,
+        move || -> io::Result<()> {
+            let mut data_guard = moving_window_handle.lock().unwrap();
+            (*data_guard).position.0 += 1;
+            drop(data_guard);
+            Ok(())
+        }
+    )));
+
+    let moving_window_handle = moving_window.get_data_handle().expect("Couldn't get window handle");
+    moving_window_controller.add_event_binding(Box::new(controller::KeyEvent::new(
+        KeyCode::Up,
+        KeyModifiers::NONE,
+        KeyEventKind::Press,
+        true,
+        move || -> io::Result<()> {
+            let mut data_guard = moving_window_handle.lock().unwrap();
+            (*data_guard).position.1 -= 1;
+            drop(data_guard);
+            Ok(())
+        }
+    )));
+
+    let moving_window_handle = moving_window.get_data_handle().expect("Couldn't get window handle");
+    moving_window_controller.add_event_binding(Box::new(controller::KeyEvent::new(
+        KeyCode::Down,
+        KeyModifiers::NONE,
+        KeyEventKind::Press,
+        true,
+        move || -> io::Result<()> {
+            let mut data_guard = moving_window_handle.lock().unwrap();
+            (*data_guard).position.1 += 1;
+            drop(data_guard);
+            Ok(())
+        }
+    )));
+
+    moving_window.attach_dispatcher(moving_window_controller);
+    widgets.push(moving_window);
 
     ///////////////
     // MAIN LOOP //
@@ -95,16 +159,22 @@ pub unsafe extern "C" fn main_loop(local: bool) -> bool {
             /* Main loop */
             loop {
                 stdout.queue(Clear(ClearType::All))?;
-                window.render(&mut stdout)?;
+                // window.render(&mut stdout)?;
+                for widget in &widgets {
+                    widget.render(&mut stdout)?;
+                }
                 stdout.flush()?;
 
                 // Event dispatching
                 if let Event::Key(event) = event::read()? {
-                    window.handle_event(&event.code, &event.kind, &event.modifiers)?;
+                    for widget in widgets.iter_mut() {
+                        widget.handle_event(&event.code, &event.kind, &event.modifiers)?;
+                    }
                 }
 
                 // check the run state of the game
-                if window.should_close() {
+                let run_state_guard = run_state.lock().unwrap();
+                if !*run_state_guard {
                     stdout.flush()?;
                     return Ok(());
                 }
@@ -114,7 +184,7 @@ pub unsafe extern "C" fn main_loop(local: bool) -> bool {
         })() /* Error handling */ {
             // failure
             Err(e) => {
-                println!("[ERROR] {e}");
+                println!("\x1b[31m[ERROR]\x1b[0m {e}");
                 clean_application()
                     .expect("\x1b[31m[ERROR]\x1b[0m Failed to clean up application");
                 false
